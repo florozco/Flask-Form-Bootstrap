14880fed754114efdf52f7c51cc8496fe3e72faa
florozco <florozco@uninorte.edu.co>
2022-05-27 17:11:19 -0500

    to use on ubuntu ssh

---
+++ application.py
@@ -0,0 +1,42 @@
+from fastai import *
+from fastai.test_utils import *
+from fastai.vision import *
+from fastai.vision.all import *
+from flask import Flask, jsonify, request
+from PIL import Image
+import albumentations as A
+
+application=Flask(__name__)
+learn = load_learner('trained_model-4.pkl')
+
+def transform_image(image_bytes):
+    image = Image.open(image_bytes)
+    my_transforms = A.Compose([
+        A.Affine(rotate=7.5, scale=1.15),
+        A.Sharpen(lightness=0.15, p=0.8),  
+        A.Perspective( pad_val=0), 
+        A.CoarseDropout(max_holes=6,min_height=5,min_width=5,max_height=20,max_width=20),
+    ], p=0.8)
+    image2 = np.array(image)
+    plt.imshow(image2)
+    image_tensor1 = my_transforms(image = image2)['image']
+    return image_tensor1
+
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    return learn.predict(tensor)
+
+@application.route('/',methods=['GET'])
+def hello():
+    hi='Hello worlds!'
+    return  jsonify({ 'class_name': hi})
+
+@application.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']        
+        return get_prediction(file)[0]
+
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
